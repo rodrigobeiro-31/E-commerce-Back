323575c3a70ea5b28fa9d5b0ddfdf870ccce6d40
Kathia01 <kathia.olaverry@gmail.com>
2023-09-11 11:48:08 -0300

    Update product seeders

--- seeders/productSeeder.js
+++ seeders/productSeeder.js
@@ -5,25 +5,236 @@ faker.locale = "es";
 
 module.exports = async () => {
   const products = [{
-    name: "Pan flauta",
+    name: "Espresso",
     price: 2.10,
-    description: "Sumérgete en la auténtica experiencia culinaria con nuestro exquisito Pan Flauta Artesanal. Elaborado con pasión y cuidado siguiendo recetas de panadería centenarias, este pan te transportará a la tradición y el sabor inigualable de la panadería de antaño.",
-    category: "panaderia",
+    description: "",
+    category: "Cafe",
     stock: 10,
-    slug: "pan-flauta",
-    image: "./img/baguette.png",
+    slug: "espresso",
+    image: "",
     top: true
   },
   {
-    name: "Espresso",
-    price: 1.50,
-    description: "Nuestro Espresso Premium es una obra de arte en cada taza, una invitación a un mundo de sabores profundos y aromas cautivadores. Cada sorbo de este elixir oscuro es una experiencia sensorial que te transportará a los rincones más apasionantes de la cultura del café.",
-    category: "cafeteria",
+    name: "Capuccino",
+    price: 2.50,
+    description: "",
+    category: "Cafe",
     stock: 20,
-    slug: "espresso",
-    image: "./img/espresso.jpg",
+    slug: "capuccino",
+    image: "",
     top: false
   },
+  {
+    name: "Hot chocolate",
+    price: 3.00,
+    description: "",
+    category: "Cafe",
+    stock: 20,
+    slug: "hot-chocolate",
+    image: "",
+    top: false
+  },
+  {
+    name: "Hot chocolate",
+    price: 3.00,
+    description: "",
+    category: "Cafe",
+    stock: 9,
+    slug: "hot-chocolate",
+    image: "",
+    top: false
+  },
+  {
+    name: "Orange chocolate cake",
+    price: 10.10,
+    description: "",
+    category: "Cakes",
+    stock: 12,
+    slug: "orange-chocolate-cake",
+    image: "",
+    top: false
+  },
+  {
+    name: "Apple pie",
+    price: 15.50,
+    description: "",
+    category: "Cakes",
+    stock: 8,
+    slug: "apple-pie",
+    image: "",
+    top: false
+  },
+  {
+    name: "Carrot cake",
+    price: 17.50,
+    description: "",
+    category: "Cakes",
+    stock: 14,
+    slug: "carrot-cake",
+    image: "",
+    top: false
+  },
+  {
+    name: "Pastafrola",
+    price: 16.10,
+    description: "",
+    category: "Cakes",
+    stock: 37,
+    slug: "pastafrola",
+    image: "",
+    top: false
+  },
+  {
+    name: "Strawberry cheesecake",
+    price: 20.00,
+    description: "",
+    category: "Cakes",
+    stock: 29,
+    slug: "strawberry-cheesecake",
+    image: "",
+    top: false
+  },
+  {
+    name: "Lemon pie",
+    price: 18.30,
+    description: "",
+    category: "Cakes",
+    stock: 25,
+    slug: "lemon-pie",
+    image: "",
+    top: false
+  },
+  {
+    name: "Baguette",
+    price: 3.90,
+    description: "",
+    category: "Bakery",
+    stock: 20,
+    slug: "baguette",
+    image: "",
+    top: false
+  },
+  {
+    name: "Croissant",
+    price: 1.60,
+    description: "",
+    category: "Bakery",
+    stock: 27,
+    slug: "croissant",
+    image: "",
+    top: true
+  },
+  {
+    name: "Quince Pastry",
+    price: 2.00,
+    description: "",
+    category: "Bakery",
+    stock: 24,
+    slug: "quince-pastry",
+    image: "",
+    top: false
+  },
+  {
+    name: "Bougnat bread",
+    price: 2.10,
+    description: "",
+    category: "Bakery",
+    stock: 34,
+    slug: "bougnat-bread",
+    image: "",
+    top: false
+  },
+  {
+    name: "Danish",
+    price: 3.30,
+    description: "",
+    category: "Bakery",
+    stock: 20,
+    slug: "danish",
+    image: "",
+    top: false
+  },
+  {
+    name: "Apple roll",
+    price: 2.50,
+    description: "",
+    category: "Bakery",
+    stock: 50,
+    slug: "apple-roll",
+    image: "",
+    top: true
+  },
+  {
+    name: "Cinnamon roll",
+    price: 2.50,
+    description: "",
+    category: "Bakery",
+    stock: 45,
+    slug: "cinnamon-roll",
+    image: "",
+    top: false
+  },
+  {
+    name: "Wholegrain bread",
+    price: 5.40,
+    description: "",
+    category: "Bakery",
+    stock: 30,
+    slug: "wholegrain-bread",
+    image: "",
+    top: false
+  },
+  {
+    name: "Macatia bread",
+    price: 4.80,
+    description: "",
+    category: "Bakery",
+    stock: 30,
+    slug: "macatia-bread",
+    image: "",
+    top: false
+  },
+  {
+    name: "Vanilla muffin",
+    price: 1.80,
+    description: "",
+    category: "Bakery",
+    stock: 30,
+    slug: "vanilla-muffin",
+    image: "",
+    top: true
+  },
+  {
+    name: "Dark roast coffee beans",
+    price: 8.90,
+    description: "",
+    category: "Coffee beans",
+    stock: 30,
+    slug: "dark-roast-coffee",
+    image: "",
+    top: false
+  },
+  {
+    name: "Medium roast coffee beans",
+    price: 8.90,
+    description: "",
+    category: "Coffee beans",
+    stock: 30,
+    slug: "medium-roast-coffee",
+    image: "",
+    top: true
+  },
+  {
+    name: "Light roast coffee beans",
+    price: 8.90,
+    description: "",
+    category: "Coffee beans",
+    stock: 30,
+    slug: "light-roast-coffee",
+    image: "",
+    top: false
+  },
+  
 ];
   await Product.insertMany(products);
   console.log("[Database] Se corrió el seeder de Products.");
