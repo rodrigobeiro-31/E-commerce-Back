af007e7c391d0f74991b773a023416cfa46874db
rodrigobeiro-31 <rodrigo.beiro@student.ha.dev>
2023-09-11 10:03:57 -0300

    .

--- E-Commerce-Back/routes/index.js
+++
@@ -1,9 +0,0 @@
-const publicRoutes = require("./publicRoutes");
-const productRoutes = require("./productRoutes");
-// const privateRoutes = require("./privateRoutes");
-
-module.exports = (app) => {
-  // app.use("/usuarios", userRoutes);
-  app.use("/products", productRoutes);
-  app.use("/", publicRoutes);
-};
--- E-Commerce-Back/routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const productController = require("../controllers/productController");
-
-router.get("/", productController.index);
-router.get("/:id", productController.show);
-
-module.exports = router;
