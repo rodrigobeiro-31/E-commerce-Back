cf88b3578495bf78e3f278ae0aeedb87812fbaa4
Leandro Matosas <lnmatosas@hotmail.com>
2023-09-14 15:43:56 -0300

    Users routes

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ const publicRoutes = require("./publicRoutes");
 const productRoutes = require("./productRoutes");
 // const privateRoutes = require("./privateRoutes");
 const authRoutes = require("./authRoutes");
+const userRoutes = require("./userRoutes");
 
 module.exports = (app) => {
   app.use("/", authRoutes);
   // app.use("/usuarios", userRoutes);
   app.use("/products", productRoutes);
+  app.use("/users", userRoutes);
   app.use("/", publicRoutes);
-
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-// router.get("/", userController.index);
+router.get("/", userController.index);
 // router.get("/crear", userController.create);
 // router.get("/:id", userController.show);
 // router.post("/", userController.store);
