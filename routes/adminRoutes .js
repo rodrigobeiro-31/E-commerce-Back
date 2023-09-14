18cce2dc7556d091e2615518cf0e4a2068b0bcd4
virulaiser <jsiutto@gmail.com>
2023-09-14 16:08:26 -0300

    ok

---
+++ routes/adminRoutes .js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const adminController = require("../controllers/adminController");
+
+router.get("/users", adminController.indexUsers);
+router.get("/products", adminController.indexProduct);
+// router.get("/crear", userController.create);
+// router.get("/:id", userController.show);
+// router.post("/", userController.store);
+// router.get("/editar/:id", userController.edit);
+// router.patch("/:id", userController.update);
+// router.delete("/:id", userController.destroy);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ const publicRoutes = require("./publicRoutes");
 const productRoutes = require("./productRoutes");
 // const privateRoutes = require("./privateRoutes");
 const authRoutes = require("./authRoutes");
+const adminRoutes = require("./adminRoutes");
 
 module.exports = (app) => {
   app.use("/", authRoutes);
   // app.use("/usuarios", userRoutes);
   app.use("/products", productRoutes);
   app.use("/", publicRoutes);
-
+  app.use("/admin", adminRoutes);
 };
