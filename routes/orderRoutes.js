1df225a91cd0ee89b75421073224eb08b110e179
Leandro Matosas <lnmatosas@hotmail.com>
2023-09-20 14:27:37 -0300

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,11 +3,14 @@ const express = require("express");
 const router = express.Router();
 const { expressjwt: checkJwt } = require("express-jwt");
 
-router.use(checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }));
+// router.use(checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }));
 
 router.get("/", orderController.index);
-router.post("/", orderController.store);
+router.post(
+  "/",
+  checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }),
+  orderController.store,
+);
 router.patch("/:id", orderController.update);
-
 
 module.exports = router;
